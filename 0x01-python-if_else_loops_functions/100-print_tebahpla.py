7b4a6d4b7362671090b7663f6ada8ea8dfc10bbc
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-01-16 15:50:52 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for letters in range(ord('z'), ord('a') - 1, -1):
-    if (letters - ord('z')) % 4 < 2:
+    curr_letter = chr(letters)
+    if (letters - ord('z')) % 2 == 0:
         print('{}'.format(chr(letters).lower()), end='')
     else:
         print('{}'.format(chr(letters).upper()), end='')
