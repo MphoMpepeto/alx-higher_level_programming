dee5f0fd8473848aac4c63e0c9a797d61702d9e0
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-01-16 13:21:39 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for n in range (10):
+for n in range(10):
     for i in range(n + 1, 10):
-        print('{:01d}{:01d}'.format(n, i), end=', ' if n < 9 else "\n")
+        print('{:01d}{:01d}'.format(n, i), end=', ' if n < 8 else "\n")
