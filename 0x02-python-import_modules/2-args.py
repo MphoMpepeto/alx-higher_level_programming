4d42180ffc8a8395867ef96baa06179eee0d03ec
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-01-18 09:25:53 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-
+if __name__== "__main__":
+    from sys import argv
+    n = len(argv) - 1
+    if n < 1:
+        print('{} arguments.'.format(n))
+    elif n == 1:
+        print('{} arguments:'.format(n))
+    else:
+        print('{} arguments:'.format(n))
+    for x in range(n):
+        print('{}: {:s}'.format(x + 1, argv[x + 1]))
