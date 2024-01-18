9e9f4ff6fd61d156d0da5965c0f7930160af2c09
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-01-18 09:43:42 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+if __name__ == '__main__':
+    from sys import argv
+    sum_arg = 0
+    for n in range(1, len(argv)):
+        sum_arg += int(argv[n])
+    print('{}'.format(sum_arg))
