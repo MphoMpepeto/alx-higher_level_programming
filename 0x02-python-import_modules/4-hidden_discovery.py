17b3640645cf341c869cd94c96971899a6110acd
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-01-18 12:33:52 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+if __name__ == "__main__":
+    from hidden_4 import *
+    hidden = dir()
+    for n in range(0, len(hidden)):
+        if hidden[n][:2] != "__":
+            print('{:s}'.format(hidden[n]))
