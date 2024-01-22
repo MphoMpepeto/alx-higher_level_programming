9e867a98ee7698c6f345234bc70af033a180635a
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-01-22 14:09:32 -0800

    print a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in matrix:
             for i in row:
-                print('{:d}'.format(i), end=' ' if i !=[-1] else '')
+                print('{:d}'.format(i), end=' ')
             print()
