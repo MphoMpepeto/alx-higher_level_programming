2053313f497bae7cc53317feee8df8e6b1c8b81f
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-01-24 16:11:50 -0800

    deletes a specific value in a dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    dicti_copy = a_dictionary.copy()
+    for x, y in dicti_copy.items():
+        if value == y:
+            a_dictionary.pop(x)
+    return a_dictionary
