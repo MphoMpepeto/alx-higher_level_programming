f24b14e6a2efb1e2f17c77251a0cf11f7a1d6382
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-02-05 12:05:33 -0800

    print list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,9 +10,10 @@ def list_division(my_list_1, my_list_2, list_length):
             division = 0
         except ZeroDivisionError:
             print("division by 0")
+            division = 0
         except IndexError:
             print("out of range")
             division = 0
         finally:
             div_list.append(division)
-            return div_list
+    return div_list
