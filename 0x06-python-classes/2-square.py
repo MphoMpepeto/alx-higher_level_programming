df6506b84182d5823181a7615fc2b57e3b288c65
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-02-06 12:47:26 -0800

    area of a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,18 +6,15 @@ class Square:
     """Defines a square qith size zero"""
 
     def __init__(self, size=0):
-
-        """Define a new square with size 0.
-
+        """ 
         Args:
-            (int) size: represents the new square size
-
-        Raises:
-            TypeError: If the given size is not an integer
-            ValueError: If the given size is less than zero
+        size of square
         """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+
+        if type(size) is int:
+            
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                raise TypeError('size must be an integer')
+            self.__size = size
