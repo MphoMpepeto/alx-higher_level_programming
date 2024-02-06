e2d7ff2324c726bc079dd79cfc06ca794e1cf377
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-02-06 10:56:37 -0800

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+"""Square module."""
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """starting a new square.
+
+        Args:
+            size (int): insert the size of the new square."""
+
+        self.__size = size
